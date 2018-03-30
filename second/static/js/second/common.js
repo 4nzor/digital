53e9af3b002cb1f207ad7b023470ead9e6e5b9a2
VikBel <vikrouz@gmail.com>
2018-03-31 00:12:03 +0300

    Add second search

--- second/static/js/second/common.js
+++ second/static/js/second/common.js
@@ -1,13 +1,59 @@
 $(document).ready(function () {
 
+    if ($(".search_checked_map input").is(':checked')) {
+        $(".search_block").addClass('search_block_active');
+        $(".search_map_block").addClass('map_block_active')
+    }
+    else {
+        $(".search_block").removeClass('search_block_active');
+        $(".search_map_block").removeClass('map_block_active')
+    }
+
+    if ($(".search_checked_advsearch input").is(':checked')) {
+        $(".search_adv_block").addClass('search_adv_active');
+        $(".search_block_submit").addClass('search_submit_active');
+        $(".search_content").addClass('search_content_active')
+    }
+    else {
+        $(".search_adv_block").removeClass('search_adv_active');
+        $(".search_block_submit").removeClass('search_submit_active');
+        $(".search_content").removeClass('search_content_active')
+    }
+
     $(".menu_button_show").click(function () {
-       $(this).hide();
-       $('.main_menu').addClass('menu_active');
+        $(this).hide();
+        $('.menu_content').addClass('menu_active');
+        $('.main_menu').addClass('menu_active');
     });
 
     $(".menu_button_hide").click(function () {
-       $(".menu_button_show").show();
-       $('.main_menu').removeClass('menu_active');
+        $(".menu_button_show").show();
+        $('.menu_content').removeClass('menu_active');
+        $('.main_menu').removeClass('menu_active');
+    });
+
+    $(".search_checked_map").click(function () {
+        if ($(".search_checked_map input").is(':checked')) {
+            $(".search_block").addClass('search_block_active');
+            $(".search_map_block").addClass('map_block_active')
+        }
+        else {
+            $(".search_block").removeClass('search_block_active');
+            $(".search_map_block").removeClass('map_block_active')
+        }
+    });
+
+    $(".search_checked_advsearch").click(function () {
+        if ($(".search_checked_advsearch input").is(':checked')) {
+            $(".search_adv_block").addClass('search_adv_active');
+            $(".search_block_submit").addClass('search_submit_active');
+            $(".search_content").addClass('search_content_active')
+        }
+        else {
+            $(".search_adv_block").removeClass('search_adv_active');
+            $(".search_block_submit").removeClass('search_submit_active');
+            $(".search_content").removeClass('search_content_active')
+        }
     });
 
 });
--- second/urls.py
+++ second/urls.py
@@ -3,11 +3,11 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('second/', views.index, name='database'),
-    path('second/search', views.search, name='search'),
-    path('second/questionnaire', views.questionnaire, name='questionnaire'),
-    path('second/about', views.about, name='about'),
-    path('second/signin', views.signin, name='signin'),
+    path('second/', views.index, name='sec_database'),
+    path('second/search', views.search, name='sec_search'),
+    path('second/questionnaire', views.questionnaire, name='sec_questionnaire'),
+    path('second/about', views.about, name='sec_about'),
+    path('second/signin', views.signin, name='sec_signin'),
 
-    path('second/profile', views.profile, name='profile'),
+    path('second/profile', views.profile, name='sec_profile'),
 ]
